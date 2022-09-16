398f4a670f2cb262c69b1d611ffb11d1db30dddf
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2022-09-16 13:50:39 +0900

    fixup! fixup! debug

--- testcases.py
+++ testcases.py
@@ -1555,7 +1555,7 @@ class MeasurementGoodput(Measurement):
 
     @staticmethod
     def repetitions() -> int:
-        return 5
+        return 1
 
     def get_paths(self):
         self._files = [self._generate_random_file(self.FILESIZE)]
