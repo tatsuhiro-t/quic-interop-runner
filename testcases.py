2690ffa5a7762e79f659b18271eafbbf05efda09
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2025-07-20 17:03:28 +0900

    Revise the way to count the number of handshakes
    Count handshakes by counting the number of HANDSHAKE_DONE per
    connection.  Previously, we assume that the number of server SCID in
    Initial packets equals to the number of handshake, but it breaks if
    server responds to multiple client initials with the different SCIDs
    in Retry packets.

--- testcases.py
+++ testcases.py
@@ -265,9 +265,18 @@ class TestCase(abc.ABC):
     def _count_handshakes(self) -> int:
         """Count the number of QUIC handshakes"""
         tr = self._server_trace()
-        # Determine the number of handshakes by looking at Initial packets.
-        # This is easier, since the SCID of Initial packets doesn't changes.
-        return len(set([p.scid for p in tr.get_initial(Direction.FROM_SERVER)]))
+        handshakes = set()
+
+        for p in tr.get_1rtt(Direction.FROM_SERVER):
+            if not hasattr(p, "frame_type") or not hasattr(p, "connection.number"):
+                continue
+
+            for f in getattr(p, "frame_type").all_fields:
+                if f.hex_value == 0x1E:
+                    handshakes.add(getattr(p, "connection.number"))
+                    break
+
+        return len(handshakes)
 
     def _get_versions(self) -> set:
         """Get the QUIC versions"""
