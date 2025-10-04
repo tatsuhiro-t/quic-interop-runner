6580c3a5e546e62e9109a6b286b1d9fe70fafcec
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2025-10-04 14:54:26 +0900

    Print server log

--- interop.py
+++ interop.py
@@ -482,6 +482,11 @@ class InteropRunner:
             shutil.copytree(client_log_dir.name, log_dir + "/client")
             shutil.copytree(sim_log_dir.name, log_dir + "/sim")
             shutil.copyfile(log_file.name, log_dir + "/output.txt")
+
+            with open(log_dir + "/server/log.txt", "r") as f:
+                content = f.read()
+                print(content)
+
             if self._save_files and status == TestResult.FAILED:
                 shutil.copytree(testcase.www_dir(), log_dir + "/www")
                 try:
